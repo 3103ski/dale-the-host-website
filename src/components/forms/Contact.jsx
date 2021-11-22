b1f2c4b61d71563684ee775f856094428a3cd5c7
3103ski <bjastski@gmail.com>
2021-11-22 14:23:37 -0800

    netlify form test

--- src/components/forms/Contact.jsx
+++ src/components/forms/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // import useForm from './useForm.js';
-// import { Form } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 
 export default function ContactForm() {
 	// const { values, onChange } = useForm(handleOnSubmit, {
@@ -14,16 +14,12 @@ export default function ContactForm() {
 	// 	alert("Message Sent! I'll get back to you as soon as I can.");
 	// }
 	return (
-		<form className='ui form' method='POST' data-netlify='true' name='contactDale'>
-			<input type='hidden' name='form-name' value='contactDale' />
-			<input type='text' name='name' placeholder='Name' />
-			<input type='text' name='email' placeholder='Email' />
-			<input type='text' name='message' placeholder='Message' />
-			<button type='submit'>Send</button>
-			{/* <Form.Input type='text' name='name' placeholder='Name' />
+		<form className='ui form' method='post' data-netlify='true' name='contact'>
+			<input type='hidden' name='form-name' value='contact' />
+			<Form.Input type='text' name='name' placeholder='Name' />
 			<Form.Input type='text' name='email' placeholder='Email' />
 			<Form.TextArea type='text' name='message' placeholder='Message' />
-			<Form.Button type='submit'>Send</Form.Button> */}
+			<Form.Button type='submit'>Send</Form.Button>
 		</form>
 	);
 }
