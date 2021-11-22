e942621c39ce4f37c75fd838c6d0f4bbe9f08744
3103ski <bjastski@gmail.com>
2021-11-22 13:54:35 -0800

    netlify form test

--- src/components/forms/Contact.jsx
+++ src/components/forms/Contact.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import useForm from './useForm.js';
 import { Form } from 'semantic-ui-react';
 
 export default function ContactForm() {
+	const { onSubmit, values, onChange, resetFormValues } = useForm(handleOnSubmit, {
+		name: '',
+		email: '',
+		message: '',
+	});
+
+	function handleOnSubmit() {
+		alert("Message Sent! I'll get back to you as soon as I can.");
+	}
 	return (
-		<Form>
+		<form className='ui form' method='post' data-netlify='true' name='contact' action='/form-success'>
+			<input type='hidden' name='form-name' value='contact' />
 			<Form.Input placeholder='Name' />
 			<Form.Input placeholder='Email' />
 			<Form.TextArea placeholder='Message' />
-			<Form.Button>Send</Form.Button>
-		</Form>
+			<Form.Button type='submit'>Send</Form.Button>
+		</form>
 	);
 }
