1ba4e7af702e15e1854ee06134eaf433f1e36320
3103ski <bjastski@gmail.com>
2021-11-22 16:04:41 -0800

    updated favicon to 512px version for fb review

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,14 @@ export default function Footer() {
 	return (
 		<Container fluid className={style.Container}>
 			<Container className={style.ContainerInner}>
-				<a href='/'>Developed By THE MEDIA STANDARD</a>
+				<a style={{ margin: '0 10px' }} href='/'>
+					Developed By THE MEDIA STANDARD
+				</a>
+				<a
+					style={{ margin: '0 10px' }}
+					href='https://www.termsfeed.com/live/bf970352-4b48-449f-9325-276f010a6337'>
+					Privacy Policy
+				</a>
 			</Container>
 		</Container>
 	);
